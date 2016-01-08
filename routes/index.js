a2c1e974650ccb37ffaa60163edf50b7cffb7241
imparikh <parikh.i.m@gmail.com>
2016-01-07 22:29:22 -0500

    first changes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
+
+/* GET Userlist page. */
+router.get('/newuser', function (req, res) {
+    res.render('newuser', {
+        "userlist": docs
+    });
+});
+
+
+router.post('/adduser', function (req, res) {
+    if (req.body.userpassword === req.body.userconfirmpassword) {
+        var ref = new Firebase('https://memokee.firebaseio.com/');
+        //need encryption
+
+    } else {
+        //passwords did not match
+    }
+}
+})
 
 module.exports = router;
