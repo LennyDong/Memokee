061fc0a75d0917e783332e6afbee26c783199555
Lenny Dong(Laptop) <lenyd@berkeley.edu>
2016-01-09 19:48:43 -0500

    More test profiles

--- public/javascripts/testProfileEntry.js
+++ public/javascripts/testProfileEntry.js
@@ -5,8 +5,57 @@ var testEntry = {
 		'30932fdc676336e89786929a1643d6a9': {
 			'salt': 'testsalt',
 			'hash': 'FmZHT+2cOA0+EjQbS65gWA==',
+			'list': {
+				'facebook': {
+					'username': 'testuser',
+					'password': 'password'
+				},
+				'twitter': {
+					'username': 'testuser',
+					'password': 'password'
+				},
+				'snapchat': {
+					'username': 'testuser',
+					'password': 'password'
+				}
+			}
+		},
+		'0c0dda9ae523b96788e0f1940c0309a3': {
+			'salt': 'testsalt',
+			'hash': 'FmZHT+2cOA0+EjQbS65gWA==',
+			'list': {
+				'facebook': {
+					'username': 'testuser2',
+					'password': 'password'
+				},
+				'twitter': {
+					'username': 'testuser2',
+					'password': 'password'
+				},
+				'snapchat': {
+					'username': 'testuser2',
+					'password': 'password'
+				}
+			}
+		},
+		'2b5d534afa366417ee628c7afb48607a': {
+			'salt': 'testsalt',
+			'hash': 'FmZHT+2cOA0+EjQbS65gWA==',
+			'list': {
+				'facebook': {
+					'username': 'testuser3',
+					'password': 'password'
+				},
+				'twitter': {
+					'username': 'testuser3',
+					'password': 'password'
+				},
+				'snapchat': {
+					'username': 'testuser3',
+					'password': 'password'
+				}
+			}
 		}
-
 	}
 }
 myFirebaseRef.set(testEntry);
